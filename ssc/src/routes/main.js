2c8a8344fe653d94cde3bd306166c67f30977b6e
csy8193 <csy8193@naver.com>
2021-03-29 17:47:36 +0900

    update

--- ssc/src/routes/main.js
+++ ssc/src/routes/main.js
@@ -7,9 +7,18 @@ class main extends Component {
       return (
         <div className="main">
           <Header></Header>
-          <div className="div">
-            
-          </div>
+          <div id="container">
+            <div className="korea_index">
+                <div className="img_kospi">코스피</div>
+                <div className="img_kosdaq">코스닥</div>
+            </div>
+            <div className="main_four_index">
+                <div className="img_four_index">4대 지표</div>
+            </div>
+            <div className="main_invest">
+                <div className="img_invest">투자 동향</div>
+            </div>
+        </div>
         </div>
       );
     }
