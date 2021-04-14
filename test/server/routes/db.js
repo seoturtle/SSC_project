620a51db6966c11f73ced28a9ed8032858f60058
csy8193 <csy8193@naver.com>
2021-04-14 22:29:22 +0900

    delete folder

--- test/server/routes/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-const db = mysql.createPool({
-    host : 'localhost',
-    user : 'root',
-    password : '1234',
-    database : 'ssc'
-});
-
-module.exports = db;
