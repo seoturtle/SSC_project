f60ed12e7dc472ef39ae9d52a95e3a9c42779a57
csy8193 <csy8193@naver.com>
2021-04-14 23:04:27 +0900

    update

--- test/src/routes/main.js
+++ test/src/routes/main.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-// import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import Header from "../components/header.js";
 import '../css/main.css';
 import jwtDecode from 'jwt-decode';
@@ -8,16 +8,22 @@ import {useCookies} from 'react-cookie';
 function Main() {
 
   //state
+  const history = useHistory();
   const [cookie, setCookie, removeCookie] = useCookies('["jwt"]');
   const [userEmail, setUserEmail] = useState("");
   const [userName, setUserName] = useState("");
 
   
   useEffect(() => {
-    const decode = jwtDecode(cookie.jwt);
-    setUserEmail(decode.email);
-    setUserName(decode.name);
-    console.log(decode);
+    if (cookie.jwt){
+      const decode = jwtDecode(cookie.jwt);
+      setUserEmail(decode.email);
+      setUserName(decode.name);
+      console.log(decode);
+    }else {
+      history.push("/")
+    }
+    
   }, []);
 
       return (
@@ -25,7 +31,7 @@ function Main() {
           <Header />
           <div id="container">
             <div className="korea_index">
-                <div className="img_kospi">{userEmail},{userName}코스피</div>
+                <div className="img_kospi">{cookie.jwt ? userEmail : null}코스피</div>
                 <div className="img_kosdaq">코스닥</div>
             </div>
             <div className="main_four_index">
