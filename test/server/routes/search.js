b298616392e99c6542f2473d390b4e1031cf9cea
csy8193 <csy8193@naver.com>
2021-04-17 01:20:39 +0900

    update

--- test/server/routes/search.js
+++ test/server/routes/search.js
@@ -9,11 +9,13 @@ router.use(cors());
 
 router.post('/', (req,res) => {
     const email = req.body.email;
-    const sql = "SELECT idx, email, name, sex FROM users where email LIKE ?"
+    const uemail = req.body.uemail;
+    const param = [email+'%', uemail]
+    const sql = "SELECT idx, email, name, sex FROM users where email LIKE ? and not email = ?"
     if(email===""){
         res.send({result: false});
     }else{
-        db.query(sql, email+'%', (err, data) => {
+        db.query(sql, param, (err, data) => {
         if(err){
             console.log(err);
         }else{
--- test/src/routes/search.js
+++ test/src/routes/search.js
@@ -1,12 +1,55 @@
 import React, { useState, useEffect} from 'react';
+import { useHistory } from "react-router-dom";
+import jwtDecode from 'jwt-decode';
+import {useCookies} from 'react-cookie';
 
 function Search() {
-
+    const history = useHistory();
     const [searchEmail, setSearchEmail] = useState([{idx: '', email: '', name: '', sex: ''}]);
+    const [cookie, setCookie, removeCookie] = useCookies('["jwt"]');
+    const [idx, setIdx] = useState("");
+    const [otherName, setOtherName] = useState("");
 
+    useEffect(() => {
+        if (cookie.jwt){
+          const decode = jwtDecode(cookie.jwt);
+          setIdx(decode.idx);
+          console.log("idx: "+decode.idx);
+        }else {
+          history.push("/");
+        }
+      }, []);
+
+    const handleClick = (e) => {
+        setOtherName(e.target.name);
+        
+        fetch("http://localhost:3002/search/add", {
+            method: "POST",
+            body: JSON.stringify({
+                idx : otherName
+              }),
+            headers: {
+              "Content-Type": "application/json"
+            }
+          })
+            .then(res=>res.json())
+            .then(res=> {
+                if(res.result==false){
+                    setSearchEmail([{idx: '', email: '', name: '', sex: ''}]);
+                }else{
+                    setSearchEmail(res.result);
+                }
+            })
+    }
+
+
+    
     const handleChange = (e) => {
+        const decode = jwtDecode(cookie.jwt);
+
         const emailinfo = {
-            email : e.target.value
+            email : e.target.value,
+            uemail : decode.email
           }
         const email_info = {
             method: "POST",
@@ -19,7 +62,7 @@ function Search() {
             .then(res=>res.json())
             .then(res=> {
                 if(res.result==false){
-                    setSearchEmail({idx: '', email: '', name: '', sex: ''});
+                    setSearchEmail([{idx: '', email: '', name: '', sex: ''}]);
                 }else{
                     setSearchEmail(res.result);
                 }
@@ -28,8 +71,14 @@ function Search() {
     return(
         <div className="search">
             <input onChange={handleChange}></input>
-            <div>{!searchEmail.map ? <div></div> : searchEmail.map(user => <div key={user.idx}>{user.email}<div>{user.name}</div></div>)}</div>
-            {console.log(searchEmail.length)}
+            <div>
+                {!searchEmail.map || searchEmail[0].idx == "" ? <div></div> : searchEmail.map(user => 
+                    <ol className="search_result" key={user.idx}>
+                        <li>{user.email}</li>
+                        <li>{user.name}</li>
+                        <button onClick={handleClick} name={user.idx} style={{height:"20px", width:"50px"}}></button>
+                    </ol>)}
+            </div>
         </div>
     )
 }
