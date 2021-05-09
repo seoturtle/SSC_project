a47c52b028ebd97265341d36715bf3bc78f0e232
csy8193 <csy8193@naver.com>
2021-05-09 20:11:28 +0900

    update

--- Choi/src/components/menu-tab/third.js
+++ Choi/src/components/menu-tab/third.js
@@ -4,7 +4,7 @@ import {useCookies} from 'react-cookie';
 import '../../css/menu-tab_css/third.css';
 
 function Third() {
-    const [cookie, setCookie, removeCookie] = useCookies('["jwt"]');
+    const [cookie] = useCookies('["jwt"]');
     const [value, setValue] = useState("");
     const [talkList, setTalkList] = useState([]);
     const decode = jwtDecode(cookie.jwt);
@@ -50,18 +50,48 @@ function Third() {
                         토론방
                     </li>
                 </ol>
-                <div>
-                <input value={value} onChange={(e) => setValue(e.target.value)}></input>
-                <input onClick={clickHandler} type='submit'></input>
-                {talkList.map(list => 
-                <div key={list.id}>
-                    <div>{list.name}</div>
-                    <div>{list.email.substring(0,3)}***</div>
-                    <div>{list.content}</div>
-                    <div>{new Date(list.date).toLocaleDateString('zh-Hans-CN')}</div>
-                    <div>{new Date(list.date).toLocaleTimeString('en-GB')}</div>
+                <div className="sto">
+                <div className="story">
+                    <div className="story_tab_list">
+                        <div className="story_tab">
+                            <div className="story_name">{decode.name}</div>
+                            <form onSubmit={clickHandler}>
+                            <input className="story_write" value={value} onChange={(e) => setValue(e.target.value)}></input>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="story_list">
+                        <div className="story_list1">
+                            {talkList.map(list => 
+                            <div className="story_list2" key={list.id}>
+                                <div className="story_head">
+                                    <div className="story_head2">
+                                        <div className="story_email">{list.email.substring(0,3)}***</div>
+                                        <div className="story_day">{new Date(list.date).toLocaleDateString('zh-Hans-CN')} {new Date(list.date).toLocaleTimeString('en-GB')}</div>
+                                    </div>
+                                    <div className="story_del"></div>
+                                </div>
+                                <div className="story_content">{list.content}</div>
+
+                                <div className="story_footer">
+                                    <button className="btn_like">
+                                        <span className="spa_like">
+                                            <span className="like_btn"></span>
+                                            <span className="like_text">1</span>
+                                        </span>
+                                    </button>
+                                    <button className="btn_hate">
+                                        <span className="spa_hate">
+                                            <span className="hate_btn"></span>
+                                            <span className="hate_text">2</span>
+                                        </span>
+                                    </button>
+                                </div>
+                            </div>
+                            )}
+                        </div>
+                    </div>
                 </div>
-                )}
                 </div>
             </div>
         );
