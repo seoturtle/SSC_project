890e90a342974b555c163428016f7d1daccd075b
csy8193 <csy8193@naver.com>
2021-05-23 05:58:02 +0900

    update

--- 210523_choi/src/components/stock_finance.js
+++ 210523_choi/src/components/stock_finance.js
@@ -18,7 +18,156 @@ function Stock_finance() {
                                 <button className="btn-year active">연간</button>
                             </div>
                         </div>
-                        <section className="financial-information-section">
+                        <div className="finance-info">
+                            <div className="years">
+                                <div></div>
+                                <div>2020/03</div>
+                                <div>2020/06</div>
+                                <div>2020/09</div>
+                                <div>2020/12</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">매출액(억)</div>
+                                <div className="value">5553,252</div>
+                                <div className="value">529,661</div>
+                                <div className="value">669,642</div>
+                                <div className="value">615,515</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">영업이익(억)</div>
+                                <div className="value">64,473</div>
+                                <div className="value">81,463</div>
+                                <div className="value">123,532</div>
+                                <div className="value">90,470</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">영업이익률(%)</div>
+                                <div className="value">11.65</div>
+                                <div className="value">15.38</div>
+                                <div className="value">18.45</div>
+                                <div className="value">14.70</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">당기순이익(억)</div>
+                                <div className="value">48,849</div>
+                                <div className="value">55,551</div>
+                                <div className="value">93,607</div>
+                                <div className="value">66,071</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">당기순이익[지배](억)</div>
+                                <div className="value">48,896</div>
+                                <div className="value">54,890</div>
+                                <div className="value">92,668</div>
+                                <div className="value">64,455</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">순이익률(%)</div>
+                                <div className="value">8.83</div>
+                                <div className="value">10.49</div>
+                                <div className="value">13.98</div>
+                                <div className="value">10.73</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">자산총계(억)</div>
+                                <div className="value">3,574,575</div>
+                                <div className="value">3,579,595</div>
+                                <div className="value">3,757,887</div>
+                                <div className="value">3,782,357</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">부채총계(억)</div>
+                                <div className="value">910,698</div>
+                                <div className="value">881,517</div>
+                                <div className="value">996,526</div>
+                                <div className="value">1,022,877</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">자본총계(억)</div>
+                                <div className="value">2,663,877</div>
+                                <div className="value">2,698,078</div>
+                                <div className="value">2,761,362</div>
+                                <div className="value">2,759,480</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">부채비율(%)</div>
+                                <div className="value">34.19</div>
+                                <div className="value">32.67</div>
+                                <div className="value">36.09</div>
+                                <div className="value">37.07</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">유동비율(%)</div>
+                                <div className="value">288.34</div>
+                                <div className="value">300.88</div>
+                                <div className="value">278.77</div>
+                                <div className="value">262.17</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">자본금(억)</div>
+                                <div className="value">8,975</div>
+                                <div className="value">8,975</div>
+                                <div className="value">8,975</div>
+                                <div className="value">8,975</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">영업활동현금흐름(억)</div>
+                                <div className="value">118,299</div>
+                                <div className="value">147,982</div>
+                                <div className="value">141,444</div>
+                                <div className="value">245,146</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">투자활동현금흐름(억)</div>
+                                <div className="value">-85,292</div>
+                                <div className="value">-24,115</div>
+                                <div className="value">-239,889</div>
+                                <div className="value">-186,991</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">재무활동현금흐름(억)</div>
+                                <div className="value">-29,736</div>
+                                <div className="value">-38,591</div>
+                                <div className="value">4,994</div>
+                                <div className="value">-19,946</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">EPS(원)</div>
+                                <div className="value">720</div>
+                                <div className="value">808</div>
+                                <div className="value">1,364</div>
+                                <div className="value">949</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">PER(배)</div>
+                                <div className="value">13.39</div>
+                                <div className="value">14.52</div>
+                                <div className="value">13.97</div>
+                                <div className="value">18.53</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">PBR(배)</div>
+                                <div className="value">1.11</div>
+                                <div className="value">1.21</div>
+                                <div className="value">1.31</div>
+                                <div className="value">1.81</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">ROE(%)</div>
+                                <div className="value">8.29</div>
+                                <div className="value">8.35</div>
+                                <div className="value">9.39</div>
+                                <div className="value">9.74</div>
+                            </div>
+                            <div className="finance-row">
+                                <div className="name">ROA(%)</div>
+                                <div className="value">6.08</div>
+                                <div className="value">6.14</div>
+                                <div className="value">6.82</div>
+                                <div className="value">7</div>
+                            </div>
+                        </div>
+                        {/* <section className="financial-information-section">
                             <table className="financial-information-table">
                                 <tbody>
                                     <td className="name">
@@ -138,7 +287,7 @@ function Stock_finance() {
                                     </td>
                                 </tbody>
                             </table>
-                        </section>
+                        </section> */}
                     </div>
                 </section>
             </div>
